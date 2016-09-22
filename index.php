d51e141ad8d9866bf0aef700a8ed66ec1410219c
vcc3 <vcc3@njit.edu>
2016-09-22 14:12:15 -0400

    add index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  echo "Hello World";
+  echo ' i am writing this to see it appear on web browser.';
+  echo 'Testing Testing Testing Testing';
+  echo' Now this is to update with my git commands and adding'
+  echo ' another test line'
+?>
